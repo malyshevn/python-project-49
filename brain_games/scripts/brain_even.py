ede5112aba569d2ee5479f1a115a87e76914ad04
Nikita Malyshev <babyswisha@mail.ru>
2023-07-21 23:07:22 +0300

    step 5

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+from random import randint
+import prompt
+
+
+
+def main():
+    print("Welcome to the Brain Games!")
+    name = prompt.string('May I have your name? ')
+    print(f"Hello, {name}")
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    count = 0
+    while count != 3:
+        random_number = randint(1, 100)
+        print(f'Question: {random_number}')
+        answer = prompt.string("Your answer ")
+        if answer == "yes" and random_number%2 == 0:
+            print("Correct")
+            count += 1
+        elif answer == "yes" and random_number%2 != 0:
+            print(f"{answer} is a wrong answer!")
+            count = 0
+            break
+        elif answer == "no" and random_number%2 != 0:
+            print("Correct!")
+        elif answer == "no" and random_number%2 == 0:
+            print(f"{answer} is a wrong answer!")
+            count = 0
+            break
+        else:
+            print(f"{answer} is a wrong answer! Try again")
+
+if __name__ == '__main__':
+    main()
